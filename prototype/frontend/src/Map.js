8a121ffba09d691980c83788b6459f8399c58ddf
Tanner Helton <tanner@tannerhelton.com>
2022-11-17 12:09:11 -0600

    Update Map.js

--- prototype/frontend/src/Map.js
+++ prototype/frontend/src/Map.js
@@ -1,11 +1,29 @@
 import * as React from 'react';
 import MapView from 'react-native-maps';
+import {Marker} from 'react-native-maps';
 import {StyleSheet, Text, View, Dimensions} from 'react-native';
 
 export default function Map() {
+  var markers = [
+    {
+      latitude: 38.95863382686692,
+      longitude: -95.24767470289626,
+      title: 'Foo Place',
+      subtitle: '1234 Foo Drive',
+    },
+  ];
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 38.95863382686692,
+          longitude: -95.24767470289626,
+          latitudeDelta: 0.02,
+          longitudeDelta: 0.02,
+        }}
+        annotations={markers}
+      />
     </View>
   );
 }
