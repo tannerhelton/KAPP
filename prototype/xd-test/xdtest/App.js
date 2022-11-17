35a6b1ba0777755e4d46e0cb9fafca1914cf2e4e
Tanner Helton <tanner@tannerhelton.com>
2022-11-17 11:35:01 -0600

    xd test

--- prototype/xd-test/xdtest/App.js
+++ prototype/xd-test/xdtest/App.js
@@ -1,4 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
 import React from 'react';
+import Component0 from './src/Component0';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -17,9 +26,6 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
 
 /* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
  * LTI update could not be added via codemod */
@@ -49,11 +55,7 @@ const Section = ({children, title}): Node => {
   );
 };
 
-const Stack = createStackNavigator();
-
-import Homepage from './src/homepage/Homepage';
-
-const App = () => {
+const App: () => Node = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -62,25 +64,35 @@ const App = () => {
 
   return (
     <SafeAreaView style={backgroundStyle}>
-      <StatusBar
+      {/* <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="Home"
-            component={Homepage}
-            options={{title: 'Home'}}
-          />
-          <Stack.Screen
-            name="Map"
-            component={Section}
-            options={{title: 'Map'}}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-      <Homepage />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.black : Colors.white,
+          }}>
+          <Section title="Step One">
+            Edit <Text style={styles.highlight}>App.js</Text> to change this
+            screen and then come back to see your edits.
+          </Section>
+          <Section title="See Your Changes">
+            <ReloadInstructions />
+          </Section>
+          <Section title="Debug">
+            <DebugInstructions />
+          </Section>
+          <Section title="Learn More">
+            Read the docs to discover what to do next:
+          </Section>
+          <LearnMoreLinks />
+        </View>
+      </ScrollView> */}
+      <Component0 />
     </SafeAreaView>
   );
 };
---
+++ prototype/xd-test/xdtest/src/Component0.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default () => {
+  return (
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "paddingStart": 76,
+        "paddingTop": 199,
+        "flex": 1
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start"
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "paddingStart": 119,
+        "paddingTop": 14,
+        "width": 270,
+        "height": 50,
+        "borderWidth": 1,
+        "borderColor": "rgba(112, 112, 112, 255)",
+        "backgroundColor": "rgba(255, 255, 255, 255)"
+      }
+    } >
+    <Text style = {
+      {
+        "fontFamily": "Helvetica Neue",
+        "fontSize": 20,
+        "color": "rgba(112, 112, 112, 255)"
+      }
+    } > test </Text>
+    </View>
+    </View>
+    </View>
+
+  );
+};
