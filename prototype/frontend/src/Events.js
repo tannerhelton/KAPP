1dc3ee6c744d94124af7272ea740f9e90235ddab
Tanner Helton <tanner@tannerhelton.com>
2022-11-18 18:38:00 -0600

    add rss feed of rockchalkcentral

--- prototype/frontend/src/Events.js
+++ prototype/frontend/src/Events.js
@@ -1,9 +1,39 @@
-import {View, Text} from 'react-native';
+import React from 'react';
+import {ScrollView, Text} from 'react-native';
+import * as rssParser from 'react-native-rss-parser';
 
-export default function Events() {
-  return (
-    <View>
-      <Text>Events</Text>
-    </View>
-  );
+export default class Events extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://rockchalkcentral.ku.edu/events.rss')
+      .then(response => response.text())
+      .then(responseData => rssParser.parse(responseData))
+      .then(rss => {
+        this.setState({
+          events: rss.items,
+        });
+      });
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        {this.state.events.map(event => (
+          <Text key={event.id}>
+            {event.title} ::
+            {event.description.substring(
+              event.description.indexOf('From') + 123,
+              event.description.indexOf('to <time') - 15,
+            )}
+          </Text>
+        ))}
+      </ScrollView>
+    );
+  }
 }
