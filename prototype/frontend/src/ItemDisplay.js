21bdcd0987d4e53d96067d2015ccc1575311ce45
Tanner Helton <tanner@tannerhelton.com>
2022-11-27 18:31:55 -0600

    location formatting done

--- prototype/frontend/src/ItemDisplay.js
+++ prototype/frontend/src/ItemDisplay.js
@@ -1,14 +1,32 @@
 import {View, Text, StyleSheet} from 'react-native';
 
 export default function ItemDisplay({eventData}) {
+  const timeData = eventData.description;
+  const locData = eventData.description;
   return (
     <View style={styles.container}>
       <Text>{eventData.title}</Text>
       <Text>
-        {eventData.description.substring(
-          eventData.description.indexOf('From') + 123,
-          eventData.description.indexOf('to <time') - 15,
+        {timeData.substring(
+          timeData.indexOf('From') + 123,
+          timeData.indexOf('to <time') - 15,
         )}
+      </Text>
+      <Text>
+        {locData
+          .substring(
+            locData.indexOf('<span class="p-location location">') + 34,
+            locData.indexOf('</span>.'),
+          )
+          .indexOf('<a') > -1
+          ? locData.substring(
+              locData.indexOf('class="url">') + 12,
+              locData.indexOf('</a></span>.'),
+            )
+          : locData.substring(
+              locData.indexOf('<span class="p-location location">') + 34,
+              locData.indexOf('</span>.'),
+            )}
       </Text>
     </View>
   );
